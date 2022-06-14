ccacec997060b306c327fd713bb1b704a902e7f8
47425 <474254903@qq.com>
2022-06-14 19:31:22 +0800

    add content replace post and comment

--- index.js
+++ index.js
@@ -218,8 +218,7 @@ async function storeReceipts(receiptsResolved, timestamp, block_height, type) {
         try {
             if (m.methodName == 'add_encrypt_content' && m.status.SuccessValue) {
                 let d = JSON.parse(m.args)
-                console.log("d :",d);
-                let hierarchies = JSON.parse(m.args).hierarchies
+                let hierarchies = d.hierarchies
                 if (hierarchies.length > 0) {
                     await asyncUtil.add_encrypt_comment(m, timestamp)
                 } else {
